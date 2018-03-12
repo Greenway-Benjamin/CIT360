9e0a7c1a14d2c3bca9d2864be502541d7b71dabd
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-03-11 22:35:21 -0600

    Add files via upload

---
+++ MVC/TheMainClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+/**
+ *
+ * @author Benjamin Greenway
+ */
+public class TheMainClass {
+    
+    public static void main (String[]args) {               
+        
+        TheModel model = new TheModel();
+        
+        TheView view = new TheView();
+        
+        TheController controller = new TheController(model, view);      
+                
+        controller.getStarted();
+        
+    }
+    
+}
